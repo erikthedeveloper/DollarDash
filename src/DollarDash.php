c2789d6d9ee25fca62475837ac1b375fed460534
Erik Aybar <erikthedeveloper@gmail.com>
2015-06-12 07:56:01 -0600

    Adds DollarDash "Kitchen Sink". Composed from all traits. Use as _

---
+++ src/DollarDash.php
@@ -0,0 +1,13 @@
+<?php namespace Eaybar\DollarDash;
+
+/**
+ * Class DollarDash - The Kitchen Sink!
+ * @package Eaybar\DollarDash
+ * @author  Erik Aybar
+ */
+class DollarDash
+{
+
+    use ArrTrait;
+    use ObjectTrait;
+}
